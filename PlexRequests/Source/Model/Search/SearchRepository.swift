0715490b26d8e4ad112a1380ff8edee7c10ccba9
Connor Lirot <ccl2of4@tx.rr.com>
2016-06-05 21:25:26 -0500

    clean up SearchRepository

--- PlexRequests/Source/Model/Search/SearchRepository.swift
+++ PlexRequests/Source/Model/Search/SearchRepository.swift
@@ -15,25 +15,25 @@ class SearchRepository: NSObject {
     var tvRequest:Alamofire.Request?
     var movieRequest:Alamofire.Request?
     
-    func searchTV(query: String, completion:[Request] -> Void) {
+    func searchTV(query: String, completion: [Request] -> Void) {
         let url = APIBaseUrl + "/tvshows"
         self.tvRequest?.cancel()
-        self.tvRequest = Alamofire.request(.GET, url, parameters: ["query": query])
-            .validate()
-            .responseArray { (response: Response<[Request], NSError>) in
-                if (response.result.error != nil) {
-                    print(response.result.error)
-                    return
-                }
-                let requests = response.result.value ?? []
-                completion(requests)
-        }
+        self.tvRequest = search(url, query: query, completion: completion)
     }
     
-    func searchMovies(query: String, completion:[Request] -> Void) {
+    func searchMovies(query: String, completion: [Request] -> Void) {
         let url = APIBaseUrl + "/movies"
         self.movieRequest?.cancel()
-        self.movieRequest = Alamofire.request(.GET, url, parameters: ["query": query])
+        self.movieRequest = search(url, query: query, completion: completion)
+    }
+    
+    func search(url: String, query: String, completion: [Request] -> Void) -> Alamofire.Request? {
+        if query.isEmpty {
+            completion([])
+            return nil
+        }
+        
+        return Alamofire.request(.GET, url, parameters: ["query": query])
             .validate()
             .responseArray { (response: Response<[Request], NSError>) in
                 if (response.result.error != nil) {
