8dc45e3f2e6737e8ed5017959caf05d2992c2991
Connor Lirot <ccl2of4@tx.rr.com>
2016-06-11 12:19:23 -0500

    do not keep Pods under source control

--- Pods/Target Support Files/Pods-PlexRequestsTests/Pods-PlexRequestsTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_PlexRequestsTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_PlexRequestsTestsVersionString[];
-
--- Pods/Target Support Files/Pods-PlexRequestsUITests/Pods-PlexRequestsUITests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_PlexRequestsUITestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_PlexRequestsUITestsVersionString[];
-
